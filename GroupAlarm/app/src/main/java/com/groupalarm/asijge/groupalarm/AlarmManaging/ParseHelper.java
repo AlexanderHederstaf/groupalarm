25af115a1afd0939a3f063aee7b337c41de93d67
joxiba <j.sehlstedt@me.com>
2015-05-19 18:14:16 +0100

    ParseHelper update

--- GroupAlarm/app/src/main/java/com/groupalarm/asijge/groupalarm/AlarmManaging/ParseHelper.java
+++ GroupAlarm/app/src/main/java/com/groupalarm/asijge/groupalarm/AlarmManaging/ParseHelper.java
@@ -2,8 +2,15 @@ package com.groupalarm.asijge.groupalarm.AlarmManaging;
 
 import android.util.Log;
 
+import com.groupalarm.asijge.groupalarm.Data.Alarm;
+import com.groupalarm.asijge.groupalarm.Data.ListRowItem;
+import com.groupalarm.asijge.groupalarm.R;
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.util.LinkedList;
@@ -19,29 +26,41 @@ public class ParseHelper {
     private static final String TABLE_GROUPS = "Groups";
 
     private static final String COLUMN_NAME = "Name";
+    private static final String COLUMN_USERS = "Users";
 
     public static void createGroup(String name) {
         final ParseObject newGroup = new ParseObject(TABLE_GROUPS);
         newGroup.put(COLUMN_NAME, name);
 
+        ParseUser user = ParseUser.getCurrentUser();
+        ParseRelation<ParseUser> relation = newGroup.getRelation(COLUMN_USERS);
+        relation.add(user);
+
         newGroup.saveInBackground(new SaveCallback() {
             public void done(ParseException e) {
                 if (e == null) {
-                    Log.d(TAG, "Alarm " + newGroup.toString() + " stored in Parse cloud.");
+                    Log.d(TAG, "Group " + newGroup.toString() + " stored in Parse cloud.");
                 } else {
-                    Log.d(TAG, "Alarm " + newGroup.toString() + " was not stored.");
+                    Log.d(TAG, "Group " + newGroup.toString() + " was not stored.");
                 }
             }
         });
     }
 
-    public static List<String> getGroupsForUser() {
-        List<String> groupList = new LinkedList<String>();
+    public static List<String> getGroupsForUser() throws ParseException {
 
-        groupList.add("Grupp1");
-        groupList.add("Grupp2");
+        ParseUser user = ParseUser.getCurrentUser();
 
-        return groupList;
+        ParseQuery<ParseObject> query = ParseQuery.getQuery(TABLE_GROUPS);
+        query.whereEqualTo(COLUMN_USERS, user);
+        List<ParseObject> groupObjectList = query.find();
+
+        List<String> groupStringList = new LinkedList<String>();
+        for (ParseObject parseObject : groupObjectList) {
+            groupStringList.add(parseObject.getString(COLUMN_NAME));
+        }
+
+        return groupStringList;
     }
 
     public static void addUserToGroup() {
