0efbbf2cc5bf322ba98df62393c7064e33a06c1e
joxiba <j.sehlstedt@me.com>
2015-05-19 16:49:18 +0100

    Added empty functions.

--- GroupAlarm/app/src/main/java/com/groupalarm/asijge/groupalarm/AlarmManaging/ParseHelper.java
+++ GroupAlarm/app/src/main/java/com/groupalarm/asijge/groupalarm/AlarmManaging/ParseHelper.java
@@ -4,5 +4,28 @@ package com.groupalarm.asijge.groupalarm.AlarmManaging;
  * Created by sehlstedt on 19/05/15.
  */
 public class ParseHelper {
-    
+
+    public static void createGroup() {
+        
+    }
+
+    public static void getGroups() {
+
+    }
+
+    public static void addUserToGroup() {
+
+    }
+
+    public static void addNewAlarmToGroup() {
+
+    }
+
+    public static void getAllRemoteAlarmsForUser() {
+
+    }
+
+    public static void userSnoozedAlarm() {
+
+    }
 }
