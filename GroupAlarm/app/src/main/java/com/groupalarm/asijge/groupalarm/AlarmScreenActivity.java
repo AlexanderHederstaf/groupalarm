c47e280e8c859af7d332f27ff1d5617ebfe7a441
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2015-05-19 15:24:29 +0200

    Javadoc and fix lambdas for AlarmScreenActivity

--- GroupAlarm/app/src/main/java/com/groupalarm/asijge/groupalarm/AlarmScreenActivity.java
+++ GroupAlarm/app/src/main/java/com/groupalarm/asijge/groupalarm/AlarmScreenActivity.java
@@ -21,11 +21,22 @@ import com.groupalarm.asijge.groupalarm.AlarmManaging.AlarmHelper;
 //TODO full screen activity
 public class AlarmScreenActivity extends Activity {
 
+    /**
+     * Debug TAG for the AlarmScreenActivity.
+     */
     public static final String TAG = "AlarmScreenActivity";
+
+    /**
+     * Default timeout for the Alarm ringing, if no response within one minute
+     * the wakelock is released.
+     */
     public static final int WAKELOCK_TIMEOUT = 60 * 1000; // 1 minute
     private PowerManager.WakeLock lock;
     private MediaPlayer mediaPlayer;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,57 +48,53 @@ public class AlarmScreenActivity extends Activity {
         Log.d(TAG, "AlarmScreen started");
 
         //Ensure wakelock release
-        Runnable releaseWakelock = new Runnable() {
+        Runnable releaseWakelock = () -> {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
-            @Override
-            public void run() {
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-
-                if (lock != null && lock.isHeld()) {
-                    lock.release();
-                }
-                if (mediaPlayer != null) {
-                    mediaPlayer.stop();
-                    mediaPlayer.release();
-                    mediaPlayer = null;
-                }
+            if (lock != null && lock.isHeld()) {
+                lock.release();
+            }
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
             }
         };
 
+        // Set up stop button to cancel sound and exit the Alarm Screen.
         Button wakeUp = (Button) findViewById(R.id.alarm_stop_button);
-        wakeUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mediaPlayer != null) {
-                    mediaPlayer.stop();
-                    mediaPlayer.release();
-                    mediaPlayer = null;
-                }
-                finish();
+        wakeUp.setOnClickListener((View v) -> {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
             }
+            finish();
         });
 
+        /* Set up snooze button to cancel sound and exit the Alarm Screen, and sheduele a new
+         * snooze alarm.
+         */
         final int snoozeTime = getIntent().getIntExtra("SNOOZE", 0);
         final Context c = this;
         Button snooze = (Button) findViewById(R.id.alarm_snooze_button);
-        snooze.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (snoozeTime > 0) {
-                    AlarmHelper.setSnooze(c, snoozeTime);
-                }
-                if (mediaPlayer != null) {
-                    mediaPlayer.stop();
-                    mediaPlayer.release();
-                    mediaPlayer = null;
-                }
-
-                finish();
+        snooze.setOnClickListener((View v) -> {
+            if (snoozeTime > 0) {
+                AlarmHelper.setSnooze(c, snoozeTime);
             }
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+
+            finish();
         });
+
+        // Disable snooze button if the time is 0 == no snooze.
         if (snoozeTime == 0) {
             snooze.setEnabled(false);
         }
@@ -95,6 +102,9 @@ public class AlarmScreenActivity extends Activity {
         new Handler().postDelayed(releaseWakelock, WAKELOCK_TIMEOUT);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onResume() {
         super.onResume();
@@ -127,6 +137,9 @@ public class AlarmScreenActivity extends Activity {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onPause() {
         super.onPause();
@@ -142,6 +155,9 @@ public class AlarmScreenActivity extends Activity {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -149,6 +165,9 @@ public class AlarmScreenActivity extends Activity {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
