515c285d944ca8e4704b1a0d3152f2714347d34d
joxiba <j.sehlstedt@me.com>
2015-05-29 09:28:59 +0100

    javadoc to Application. Removed parse app Id.

--- GroupAlarm/app/src/main/java/com/groupalarm/asijge/groupalarm/Application.java
+++ GroupAlarm/app/src/main/java/com/groupalarm/asijge/groupalarm/Application.java
@@ -4,6 +4,8 @@ import com.parse.Parse;
 import com.parse.ParseInstallation;
 
 /**
+ * Application.java is a singleton running over the whole lifecycle of the GroupAlarm app.
+ * Can be used to create any variables that need global access.
  *
  * @author asijge
  */
@@ -11,9 +13,17 @@ public class Application extends android.app.Application {
 
     @Override
     public void onCreate() {
+
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "FId7HlOrHpQ3O8UCZkZtUP6vdxCR0T0DfFgbRitf", "6jgsndZW6hvsVHenBLpuQ7egH6HPjGPIBPqn89A1");
+
+        // Initialise Parse.com app Id and Client key
+        // ****************************************************************************
+        // Uncomment and fill in with your Parse credentials:
+        // Parse.initialize(this, "your_application_id", "your_client_key");
+        // ****************************************************************************
+
+        // Save details of the current app installation to the Parse cloud
         ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 }
